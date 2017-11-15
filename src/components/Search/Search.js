4fd7a9c0616468765d4e2c3f182e532071cc3240
DuyDo <duydo.dev@gmail.com>
2017-11-15 01:31:36 -0600

    finish wireframe

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -5,17 +5,17 @@ class Form extends Component {
     render() {
         return(
             <form className="form">
-                <h4 className="text-center">Topic</h4>
+                <h4>Topic</h4>
                 <div className="">
                     <input type="text" className="form-control" />
                 </div>
 
-                <h4 className="text-center">Start year</h4>
+                <h4>Start year</h4>
                 <div className="">
                     <input type="text" className="form-control" />
                 </div>
 
-                <h4 className="text-center">End year</h4>
+                <h4>End year</h4>
                 <div className="">
                     <input type="text" className="form-control" />
                 </div>
@@ -29,7 +29,7 @@ class Search extends Component {
     render() {
         return (
             <div className="panel panel-primary">
-                <div className="panel-heading text-center"><h3>Search</h3></div>
+                <div className="panel-heading"><h3>Search</h3></div>
                 <div className="panel-body">
                     <Form />
                 </div>
